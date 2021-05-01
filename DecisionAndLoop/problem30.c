e0d5723389143d5971416aa438c04e83d643b9c4
pranavvadanere <pranav.vadanere@gmail.com>
2021-05-01 20:33:41 +0530

    pattern prog

--- DecisionAndLoop/problem30.c
+++ DecisionAndLoop/problem30.c
@@ -1,8 +1,19 @@
-// pattern
-
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
-	
+	int i,j,space;
+
+	for(i = 5; i >= 1; i--){
+		for(space = 5; space > i;space--){
+			printf(" ");
+		}
+		for(j = 1; j <=i;j++){
+			printf("%c",123-j);
+		}
+		for(j = j-2; j >= 1; j--){
+			printf("%c",123-j);
+		}
+		printf("\n");
+	}
 	return 0;
 }
